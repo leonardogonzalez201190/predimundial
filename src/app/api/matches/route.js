44c8dba643f2ac52c6f6063e77a7f70f9c1154e6
leonardogonzalez201190 <leonardogonzalez201190@gmail.com>
2026-01-20 20:26:24 +0100

    results changes

--- src/app/api/matches/route.js
+++ src/app/api/matches/route.js
@@ -18,11 +18,11 @@ const data = {
         },
         {
           id: "A2",
-          date: "2026-06-12",
+          date: "2024-06-12",
           time: "16:00",
           venue: "Mexico City, MX",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "Mexico", code: "MX", flagUrl: "https://flagcdn.com/mx.svg" },
           away: { name: "Poland", code: "PL", flagUrl: "https://flagcdn.com/pl.svg" }
         }
@@ -33,21 +33,21 @@ const data = {
       matches: [
         {
           id: "B1",
-          date: "2026-06-11",
+          date: "2024-06-11",
           time: "22:00",
           venue: "Toronto, ON",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "England", code: "GB-ENG", flagUrl: "https://flagcdn.com/gb-eng.svg" },
           away: { name: "Senegal", code: "SN", flagUrl: "https://flagcdn.com/sn.svg" }
         },
         {
           id: "B2",
-          date: "2026-06-13",
+          date: "2024-06-13",
           time: "18:00",
           venue: "Edmonton, AB",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "USA", code: "US", flagUrl: "https://flagcdn.com/us.svg" },
           away: { name: "Senegal", code: "SN", flagUrl: "https://flagcdn.com/sn.svg" }
         }
@@ -58,41 +58,41 @@ const data = {
       matches: [
         {
           id: "C1",
-          date: "2026-06-13",
+          date: "2024-06-13",
           time: "18:00",
           venue: "New York/New Jersey, NY",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "Brazil", code: "BR", flagUrl: "https://flagcdn.com/br.svg" },
           away: { name: "Morocco", code: "MA", flagUrl: "https://flagcdn.com/ma.svg" }
         },
         {
           id: "C2",
-          date: "2026-06-13",
+          date: "2024-06-13",
           time: "21:00",
           venue: "Boston, MA",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "Haiti", code: "HT", flagUrl: "https://flagcdn.com/ht.svg" },
           away: { name: "Scotland", code: "GB-SCT", flagUrl: "https://flagcdn.com/gb-sct.svg" }
         },
         {
           id: "C3",
-          date: "2026-06-17",
+          date: "2024-06-17",
           time: "20:00",
           venue: "Philadelphia, PA",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "Morocco", code: "MA", flagUrl: "https://flagcdn.com/ma.svg" },
           away: { name: "Haiti", code: "HT", flagUrl: "https://flagcdn.com/ht.svg" }
         },
         {
           id: "C4",
-          date: "2026-06-17",
+          date: "2024-06-17",
           time: "23:00",
           venue: "Atlanta, GA",
-          status: "scheduled",
-          result: null,
+          status: "finished",
+          result: { "home": 2, "away": 1 },
           home: { name: "Brazil", code: "BR", flagUrl: "https://flagcdn.com/br.svg" },
           away: { name: "Scotland", code: "GB-SCT", flagUrl: "https://flagcdn.com/gb-sct.svg" }
         }
